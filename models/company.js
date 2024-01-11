5b484c9061212077711798f8583b717a595ae7b1
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-10 18:26:27 -0600

    added function search to models

--- models/company.js
+++ models/company.js
@@ -67,6 +67,23 @@ class Company {
     return companiesRes.rows;
   }
 
+  static async search(filterParams) {
+    const name = filterParams.companyName || "";
+    const minEmployees = filterParams.minEmployees || 0;
+    const maxEmployees = filterParams.maxEmployees || Infinity;
+
+    const companiesRes = await db.query(
+    `SELECT handle,
+            name,
+            description,
+            num_employees AS "numEmployees",
+            logo_url AS "logoUrl"
+            FROM companies
+            WHERE name ILIKE $1 AND num_employees >= $2 AND num_employees <= $3
+            ORDER BY name`,
+            [`'%${name}%'`, `${minEmployees}`, `${maxEmployees}`]);
+
+  }
   /** Given a company handle, return data about company.
    *
    * Returns { handle, name, description, numEmployees, logoUrl, jobs }
